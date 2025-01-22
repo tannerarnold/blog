ec519bbf3f8ab658a3e5775de79634177a227f4a
Tanner Arnold <tanner.arnold.tech@gmail.com>
2025-01-22 14:57:54 -0700

    fix: use lower version of eslint for super-linter, ignore the migration folder for linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  ignorePatterns: ['dist', 'node_modules', 'src/lib/db/migrations'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
